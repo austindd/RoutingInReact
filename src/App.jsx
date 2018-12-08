bc60b7ed0ec696308db85a7fd78a558a6346d67a
Austin Davis <austindanieldavis@gmail.com>
2018-12-08 03:05:17 -0600

    One last style change

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,15 @@ class App extends Component {
           <>
             <AppHeader>
               <DivWrapper style={{alignSelf: 'flex-end'}}>
-                <LinkBtn2 to='/home' style={{width: '10em', height: '3vh'}}>Home</LinkBtn2> {/* 'LinkButton' is a custom version of the <Link> component that renders like a button */}
                 <LinkBtn2 to='/films' style={{width: '10em', height: '3vh'}}>Films</LinkBtn2> {/* 'LinkBtn1' and 'LinkBtn2' are styled versions of the <LinkButton> */}
+                <LinkBtn2 to='/home' style={{width: '10em', height: '3vh'}}>Home</LinkBtn2> {/* 'LinkButton' is a custom version of the <Link> component that renders like a button */}
                 <LinkBtn2 to='/people' style={{width: '10em', height: '3vh'}}>People</LinkBtn2> {/* See 'LinkButton.jsx' and 'AppTheme.jsx' for details */}
               </DivWrapper>
             </AppHeader>
             <AppBody>
               <Switch>
-                <Route exact path="/home" render={(props) => <Home {...props} />} /> {/* 'render()' allows us to pass props into components through a 'Route' */}
                 <Route exact path="/films" render={(props) => <Films {...props} />} />
+                <Route exact path="/home" render={(props) => <Home {...props} />} /> {/* 'render()' allows us to pass props into components through a 'Route' */}
                 <Route exact path="/people" render={(props) => <People {...props} />} />
               </Switch>
             </AppBody>
